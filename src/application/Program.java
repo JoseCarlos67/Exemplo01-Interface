ec1ba53366335ae5824159965d5b47fb07a9e17f
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-11-23 19:55:59 -0300

    Servico de tax e geracao da fatura

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -25,5 +27,22 @@ public class Program {
     LocalDateTime finish = LocalDateTime.parse(scanner.nextLine(), fmt);
 
     CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+
+    System.out.print("Entre com o preço por hora: ");
+    double pricePerHour = scanner.nextDouble();
+
+    System.out.print("Entre com o preço por dia: ");
+    double pricePerDay = scanner.nextDouble();
+
+
+    BrazilTaxService brazilTaxService = new BrazilTaxService();
+    RentalService rentalService = new RentalService(pricePerHour, pricePerDay, brazilTaxService);
+
+    rentalService.processInvoice(carRental);
+
+    System.out.println("FATURA:");
+    System.out.println("Pagamento básico: " + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+    System.out.println("Imposto: " + String.format("%.2f", carRental.getInvoice().getTax()));
+    System.out.println("Pagamento total: " + String.format("%.2f", carRental.getInvoice().getTotalPayment()));
   }
 }
